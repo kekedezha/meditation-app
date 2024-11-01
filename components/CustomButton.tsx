6b46bd93e6d51a2b8270b0b2a2d651f36689b658
kekedezha <dezhac6@gmail.com>
2024-10-31 22:16:29 -0400

    finished custombutton component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -8,10 +8,19 @@ interface CustomButtonProps {
   containerStyles?: string;
 }
 
-const CustomButton = () => {
+const CustomButton = ({
+  onPress,
+  title,
+  textStyles = "",
+  containerStyles = "",
+}: CustomButtonProps) => {
   return (
-    <TouchableOpacity>
-      <Text>CustomButton</Text>
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className={`bg-white rounded-xl min-h-[62px] justify-center items-center ${containerStyles}`}
+      onPress={onPress}
+    >
+      <Text className={`font-semibold text-lg ${textStyles}`}>{title}</Text>
     </TouchableOpacity>
   );
 };
