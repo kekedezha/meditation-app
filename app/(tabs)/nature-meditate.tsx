8b5e29bd666959bc94f838c341d6a1a9cbb2b768
kekedezha <dezhac6@gmail.com>
2024-11-25 21:07:30 -0500

    updated nature-meditation.tsx file

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Pressable, ImageBackground } from "react-native";
 import React from "react";
+import AppGradient from "@/components/AppGradient";
+import { StatusBar } from "expo-status-bar";
+
+import { MEDITATION_DATA } from "@/constants/MeditationData";
+import MEDITATION_IMAGES from "@/constants/meditation-images";
 
 const NatureMeditate = () => {
   return (
-    <View>
-      <Text>NatureMeditate</Text>
+    <View className="flex-1">
+      <AppGradient colors={["#161b2e, #0a4d4a, #766e67"]}>
+        <View className="mb-6">
+          <Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
+            Welcome Jordan
+          </Text>
+          <Text className="text-indigo-100 text-xl font-medium">
+            Start your mediation practice today
+          </Text>
+        </View>
+        <View>
+          <FlatList
+            data={MEDITATION_DATA}
+            className="mb-20"
+            keyExtractor={(item) => item.id.toString()}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Pressable
+                onPress={() => console.log("press")}
+                className="h-48 my-3 rounded-md overflow-hidden"
+              >
+                <ImageBackground
+                  source={MEDITATION_IMAGES[item.id - 1]}
+                  resizeMode="cover"
+                  className="flex-1 rounded-lg justify-center"
+                >
+                  <Text>{item.title}</Text>
+                </ImageBackground>
+              </Pressable>
+            )}
+          />
+        </View>
+      </AppGradient>
+      <StatusBar style="light" />
     </View>
   );
 };
