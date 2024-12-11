226b73bdd40d5b1f0e7c1a868e71a5562fa487db
kekedezha <dezhac6@gmail.com>
2024-12-10 21:00:58 -0500

    added stack screens to app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
-export default function Root() {
-  return <Slot />;
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
