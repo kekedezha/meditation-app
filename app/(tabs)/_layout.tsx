f2e34017d5ec42c1200e7be0b405707fbb0f8bb3
kekedezha <dezhac6@gmail.com>
2024-12-10 21:05:34 -0500

    added affirmations tab to tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import Colors from "@/constants/Colors";
+import { MaterialCommunityIcons, Entypo } from "@expo/vector-icons";
 
 const TabsLayout = () => {
   return (
@@ -13,7 +14,25 @@ const TabsLayout = () => {
     >
       <Tabs.Screen
         name="nature-meditate"
-        options={{ tabBarLabel: "Meditate" }}
+        options={{
+          tabBarLabel: "Meditate",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="flower-tulip"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="affirmations"
+        options={{
+          tabBarLabel: "Affirmations",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="open-book" size={24} color={color} />
+          ),
+        }}
       />
     </Tabs>
   );
