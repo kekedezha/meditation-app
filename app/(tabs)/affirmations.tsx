0947e23aeeab8d01af832a6f04dde802ad38e344
kekedezha <dezhac6@gmail.com>
2024-12-11 20:49:07 -0500

    updated gradient background to affirmations screen/comp

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import AppGradient from "@/components/AppGradient";
 
 const Affirmations = () => {
   return (
-    <View>
-      <Text>Affirmations</Text>
+    <View className="flex-1">
+      <AppGradient colors={["#2e1f5a", "#54426b", "a790af"]}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text className="text-zinc-50 text-3xl font-bold">
+            Change your beliefs with affirmations
+          </Text>
+          <View></View>
+        </ScrollView>
+      </AppGradient>
     </View>
   );
 };
