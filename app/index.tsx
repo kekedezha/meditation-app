3f7b0a98c3fdf459d86608d9562a24035a867552
kekedezha <dezhac6@gmail.com>
2025-01-08 16:38:24 -0500

    updated welcome screen text position

--- app/index.tsx
+++ app/index.tsx
@@ -17,7 +17,7 @@ const App = () => {
       >
         <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
           <SafeAreaView className="flex-1 px-1 justify-between">
-            <View>
+            <View className="mt-48">
               <Text className="text-center text-white font-bold text-4xl">
                 Simple Meditation
               </Text>
